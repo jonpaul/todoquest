4b13116300609fac45bddaa0cc90ec383cfd6f08
Jon-Paul Lussier Jr <jp@rumbrook.net>
2012-05-07 22:22:49 -0400

    Initial rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoquest::Application.config.secret_token = '2188e13acfcad4ba770fe40400bf09e7196789430204a430ddc5d70e4719b986b35640e3db5abbf55c8523b432fdeefab62271bcfa9a4a37659bff97617a6a31'
